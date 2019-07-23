c8111baf94c02f248fdb3335f5f609d762d4e9c5
kuppalla9 <52877289+kuppalla9@users.noreply.github.com>
2019-07-23 16:12:38 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
       System.out.println("Hello all, Welcome to day 8 change some thing 2 time changingclass Devops Team");
-      System.out.println("Hi siva how was today aaachanging 3 time");
+      System.out.println("Hi siva how was today changing on tuesday time");
    }
 }
